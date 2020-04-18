9309e23dbbd0d2a4b3508e21616b72df6a3ec572
Waqar-107 <waqar.hassan866@gmail.com>
2020-04-19 01:21:08 +0600

    lang toggler handled

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -4,12 +4,53 @@ import PropTypes from "prop-types";
 
 import {AuthConsumer} from "../../stateHandlers/authContext";
 
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import TranslateIcon from "@material-ui/icons/Translate";
 import styles from "./NavbarStyles";
 
 class Template extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			anchorEl: null,
+		};
+	}
+
+	languageToggler = (lang) => {
+		const {setLanguage} = this.props;
+		setLanguage(lang, () => {});
+	};
+
+	handleClick = (event) => {
+		this.setState({anchorEl: event.currentTarget});
+	};
+
+	handleClose = (lang) => {
+		this.languageToggler(lang);
+		this.setState({anchorEl: null});
+	};
+
 	render() {
 		const {classes} = this.props;
-		return <div className={classes.mainBody}></div>;
+		const {anchorEl} = this.state;
+
+		return (
+			<div className={classes.mainBody}>
+				<React.Fragment>
+					<TranslateIcon className={classes.langBtn} onClick={this.handleClick} />
+					<Menu
+						id="simple-menu"
+						anchorEl={anchorEl}
+						keepMounted
+						open={Boolean(anchorEl)}
+						onClose={this.handleClose}>
+						<MenuItem onClick={() => this.handleClose("bn")}>বাংলা</MenuItem>
+						<MenuItem onClick={() => this.handleClose("en")}>English</MenuItem>
+					</Menu>
+				</React.Fragment>
+			</div>
+		);
 	}
 }
 
@@ -19,7 +60,7 @@ Template.propTypes = {
 
 const ConsumerComponent = (props) => (
 	<AuthConsumer>
-		{({testVariable}) => <Template {...props} testVariable={testVariable} />}
+		{({setLanguage}) => <Template {...props} setLanguage={setLanguage} />}
 	</AuthConsumer>
 );
 
--- src/app/components/Navbar/NavbarStyles.js
+++ src/app/components/Navbar/NavbarStyles.js
@@ -3,6 +3,32 @@ const styles = (theme) => ({
 		width: "100%",
 		height: "70px",
 		background: theme.palette.colors.primary,
+
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		alignContent: "center",
+	},
+
+	langBtn: {
+		fontSize: "25px",
+		color: "white",
+	},
+
+	langMenu: {
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+		alignContent: "center",
+	},
+
+	langMenuItem: {
+		paddingLeft: "10px",
+		paddingRight: "10px",
+		paddingTop: "5px",
+		paddingBottom: "5px",
 	},
 });
 
